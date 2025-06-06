050eb4c395ab901fbe84d5543ad3f4c0d0efe176
asytka <asyt2203@gmail.com>
2025-06-06 21:46:04 +0300

    added announcement model and controller, added swagger for api testing and created new database on ssms

---
+++ Data/Announcement_apiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Announcement_api.Model;
+
+namespace Announcement_api.Data
+{
+    public class Announcement_apiContext : DbContext
+    {
+        public Announcement_apiContext (DbContextOptions<Announcement_apiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Announcement_api.Model.Announcement> Announcement { get; set; } = default!;
+    }
+}
---
+++ Model/Announcement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Announcement_api.Model
+{
+    public class Announcement
+    {
+        [Key]
+        public int id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateOnly Date { get; set; }
+    }
+}
